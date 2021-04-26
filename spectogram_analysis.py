1b5f18b4c1b7df441e8a3ddcf3c1480a36aabaf8
Filip Šašić <34026980+FilipSasic@users.noreply.github.com>
2021-04-26 10:00:56 +0200

    Update spectogram_analysis.py
    Changed spectogram to mel_spectogram and added some paramaters

--- spectogram_analysis.py
+++ spectogram_analysis.py
@@ -7,16 +7,15 @@ import librosa.display
 def create_spectocram_image(audio_file):
     clip, sample_rate = librosa.load(audio_file, sr=None)
 
-    D = librosa.stft(clip)
-    S_db = librosa.amplitude_to_db(np.abs(D), ref=np.max)
+    # D = librosa.stft(clip)
+    # S_db = librosa.amplitude_to_db(np.abs(D), ref=np.max)
 
-    # fig, ax = plt.subplots()
-    # img = librosa.display.specshow(S_db, ax=ax)
-    # fig.colorbar(img, ax=ax)
+    M = librosa.feature.melspectrogram(y=clip, sr=sample_rate, hop_length=256, n_fft=4096)
+    M_db = librosa.power_to_db(M, ref=np.max)
 
     fig, ax = plt.subplots()
-    img = librosa.display.specshow(S_db, x_axis='time', y_axis='log', ax=ax)
-    ax.set(title='Using a logarithmic frequency axis')
+    img = librosa.display.specshow(M_db,hop_length=256,x_axis='time', y_axis='mel', ax=ax)
+    ax.set(title='Mel spectrogram display')
     fig.colorbar(img, ax=ax, format="%+2.f dB")
 
     plt.savefig("spectogram.png")
